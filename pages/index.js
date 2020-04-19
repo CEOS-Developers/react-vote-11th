56633f363d36e7cd83308fa1b315f661995b927c
seungwon2 <dandelionsw2@gmail.com>
2020-04-19 10:13:00 +0900

    LoginCheck 함수 제거,  네이밍 수정

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,17 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 
-import MemorizedLoginForm from "../src/components/login-form";
-import MemorizedVoteForm from "../src/components/vote-form";
+import LoginForm from "../src/components/login-form";
+import VoteForm from "../src/components/vote-form";
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginCheck = () => {
-    console.log("login check start");
-    if (isLoggedIn) {
-      console.log("dd");
-      return <MemorizedVoteForm />;
-    } else {
-      return (
-        <MemorizedLoginForm
-          isLoggedIn={isLoggedIn}
-          setIsLoggedIn={setIsLoggedIn}
-        />
-      );
-    }
-  };
-
   return (
     <Wrapper>
       <Title>리액트 투-표</Title>
-      {loginCheck()}
+      {!isLoggedIn && <LoginForm {...{ setIsLoggedIn }} />}
+      {isLoggedIn && <VoteForm />}
     </Wrapper>
   );
 }
